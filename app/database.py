e8133fda2d570b69f8fb81f317c6a3ff63e96dfa
Maksym Yelisyeyev <myelisyeyev9498@sdsu.edu>
2025-06-13 10:16:06 -0700

    Switching to PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from databases import Database
 
-DATABASE_URL = "sqlite:///./mates.db"
+DATABASE_URL = "postgresql://mates_db_user:7m8lP4Xx6G58F4Eg38CPxRcDWXkdaL5o@dpg-d165nu63jp1c73fbags0-a.oregon-postgres.render.com/mates_db"
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
