b198834b8ca8d87692f03c52359baa48461d8ebd
Maksym Yelisyeyev <myelisyeyev9498@sdsu.edu>
2025-06-13 11:52:26 -0700

    Fix psycopg2 error for PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,11 +1,13 @@
-from sqlalchemy import create_engine, MetaData
-from databases import Database
+from sqlalchemy import create_engine
+import os
 
-DATABASE_URL = "postgresql://mates_db_user:7m8lP4Xx6G58F4Eg38CPxRcDWXkdaL5o@dpg-d165nu63jp1c73fbags0-a.oregon-postgres.render.com/mates_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-database = Database(DATABASE_URL)
-metadata = MetaData()
-
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Detect if using SQLite (for local dev)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
